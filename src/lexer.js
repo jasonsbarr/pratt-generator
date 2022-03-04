ab2289e71f63928bc9b34b114356c709b04c584f
Jason Barr <jason.barr@metronetinc.com>
2022-03-04 17:16:50 -0600

    Add token rules for binary expressions

--- src/lexer.js
+++ src/lexer.js
@@ -1,1 +1,14 @@
-import {rule, lexer} from "@jasonsbarr/lexer";
+import { rule, lexer } from "@jasonsbarr/lexer";
+
+const rules = [
+  rule("Number", "NUMBER", String.raw`\d+`),
+  rule("Symbol", "EXP", String.raw`\*\*`),
+  rule("Symbol", "PLUS", String.raw`\+`),
+  rule("Symbol", "MINUS", String.raw`\-`),
+  rule("Symbol", "MUL", String.raw`\*`),
+  rule("Symbol", "DIV", String.raw`\/`),
+];
+
+const lex = lexer(rules);
+
+export const tokenize = (input) => lex.compile().input(input).tokenize();
