86a5e9a4ab7df218197cb3a1ae610736db1ea83a
Jason Barr <jason.barr@metronetinc.com>
2022-03-04 17:55:01 -0600

    Fix token regex

--- src/lexer.js
+++ src/lexer.js
@@ -1,14 +1,18 @@
 import { rule, lexer } from "@jasonsbarr/lexer";
 
 const rules = [
+  rule("WS", "WS", String.raw`\s+`),
   rule("Number", "NUMBER", String.raw`\d+`),
-  rule("Symbol", "EXP", String.raw`\*\*`),
   rule("Symbol", "PLUS", String.raw`\+`),
-  rule("Symbol", "MINUS", String.raw`\-`),
+  rule("Symbol", "MINUS", String.raw`-`),
+  rule("Symbol", "EXP", String.raw`\*\*`),
   rule("Symbol", "MUL", String.raw`\*`),
-  rule("Symbol", "DIV", String.raw`\/`),
+  rule("Symbol", "DIV", String.raw`/`),
 ];
 
 const lex = lexer(rules);
 
-export const tokenize = (input) => lex.compile().input(input).tokenize();
+const filterWs = (tokens) => [...tokens].filter((t) => t.type !== "WS");
+
+export const tokenize = (input) =>
+  filterWs(lex.compile().input(input).tokenize());
