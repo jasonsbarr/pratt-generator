b953a8751269f9a849659bd618ba881b6ca3bcea
Jason Barr <jason.barr@metronetinc.com>
2022-03-04 19:52:26 -0600

    Test parser generation

---
+++ test.js
@@ -0,0 +1,83 @@
+import { tokenize } from "./src/lexer.js";
+import { createParser } from "./src/parser.js";
+
+const symbols = [
+  {
+    type: "Literal",
+    name: "NUMBER",
+    prec: 0,
+    den: "NUD",
+    prec: 0,
+    assoc: "NONE",
+    arity: "NULL",
+  },
+  {
+    type: "Symbol",
+    name: "PLUS",
+    prec: 30,
+    den: "LED",
+    assoc: "LEFT",
+    arity: "BINARY",
+  },
+  {
+    type: "Symbol",
+    name: "PLUS",
+    prec: 45,
+    den: "NUD",
+    assoc: "RIGHT",
+    arity: "UNARY",
+  },
+  {
+    type: "Symbol",
+    name: "MINUS",
+    prec: 30,
+    den: "LED",
+    assoc: "LEFT",
+    arity: "BINARY",
+  },
+  {
+    type: "Symbol",
+    name: "MINUS",
+    prec: 45,
+    den: "NUD",
+    assoc: "RIGHT",
+    arity: "UNARY",
+  },
+  {
+    type: "Symbol",
+    name: "MUL",
+    prec: 35,
+    den: "LED",
+    assoc: "LEFT",
+    arity: "BINARY",
+  },
+  {
+    type: "Symbol",
+    name: "DIV",
+    prec: 35,
+    den: "LED",
+    assoc: "LEFT",
+    arity: "BINARY",
+  },
+  {
+    type: "Symbol",
+    name: "EXP",
+    prec: 40,
+    den: "LED",
+    assoc: "RIGHT",
+    arity: "BINARY",
+  },
+  {
+    type: "EndOfInput",
+    name: "ENDOFINPUT",
+    prec: -1,
+    den: "NUD",
+    prec: 0,
+    assoc: "NONE",
+    arity: "NULL",
+  },
+];
+
+const parser = createParser(symbols);
+
+export const parse = (input) => parser(tokenize(input));
