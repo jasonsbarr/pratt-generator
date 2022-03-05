9404a04398cba92fdbde81f21b9d3bbe26048e06
Jason Barr <jason.barr@metronetinc.com>
2022-03-04 19:30:27 -0600

    1st attempt at parser generator

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,68 @@
-import { tokenize } from "./lexer.js";
+export const createParser = (syms) => {
+  const symbols = {};
+  const assoc = { NONE: 0, LEFT: 0, RIGHT: 1 };
+  const nud = {};
+  const led = {};
+  const setTokenAtts = ({
+    type,
+    name,
+    den = "NUD",
+    prec = 0,
+    assoc = "NONE",
+    arity = "NULL",
+  }) => ({
+    type,
+    name,
+    den,
+    prec,
+    assoc,
+    arity,
+  });
+  const registerSymbol = (sym) => (symbols[sym.name] = setTokenAtts(sym));
 
-let rules = {};
+  return (tokens) => {
+    let pos = 0;
 
-export const parse = (input) => tokenize(input);
+    const next = () => tokens[++pos];
+    const peek = () => tokens[pos];
+
+    const binop = (name, bp) => (left) => {
+      const op = symbols[name];
+      return { left, op, right: parseExpr(bp - assoc[op.assoc]) };
+    };
+    const unop = (name, bp) => () => ({
+      op: symbols[name],
+      expr: parseExpr(bp),
+    });
+
+    const parseExpr = (rbp = 0) => {
+      let token = peek();
+      let left = nud[token.name]();
+      token = next();
+
+      while (rbp < symbols[token.name].prec) {
+        let t = token;
+        token = next();
+        left = led[t.name](left);
+      }
+
+      return left;
+    };
+
+    for (let s of syms) {
+      registerSymbol(s);
+
+      if (s.den === "NUD") {
+        if (s.arity === "NULL") {
+          nud[s.name] = () => s;
+        } else if (s.arity === "UNARY") {
+          nud[s.name] = unop(s.name, s.prec);
+        }
+      } else if (s.den === "LED") {
+        if (s.arity === "BINARY") {
+          led[s.name] = binop(s.name, s.prec);
+        }
+      }
+    }
+  };
+};
