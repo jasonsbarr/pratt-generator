4636e397204e77176bd0f63335866a19d1f045d9
Jason Barr <jason.barr@metronetinc.com>
2022-03-06 19:05:46 -0600

    First attempt at rules parser

--- src/parser.js
+++ src/parser.js
@@ -14,6 +14,7 @@ export const createParser = (
 ) => {
   const ops = {};
   const parseRules = {};
+  const dispatchRules = {};
   const seqOps = [];
   const terms = [];
   const assigns = [];
--- src/rule_parser.js
+++ src/rule_parser.js
@@ -16,3 +16,32 @@ const isOpt = (rule) => rule.includes("|");
 const splitOpts = (rule) => rule.slice(1, -1).split("|");
 const hasField = (rule) => rule.includes(":");
 const getFieldRule = (rule) => rule.split(":"); // returns array pair for destructuring
+
+const makeRulePart = (word) => {
+  const [field, ruleStr] = hasField(word) ? getFieldRule(word) : [null, word];
+  const [required, rule] = isKleene(ruleStr)
+    ? [getKleene(ruleStr), ruleStr.slice(0, -1)]
+    : ["yes", ruleStr];
+  const options = isOpt(ruleStr) ? splitOpts(ruleStr) : null;
+  const name = options ? null : ruleStr;
+  const terminal = isTerminal(ruleStr);
+
+  return { field, required, rule, options, name, terminal };
+};
+
+const makeRule = (ruleArr) => {
+  const [name, ...rest] = ruleArr;
+  return [name, rest.map(makeRulePart)];
+};
+
+export const parseRules = (rules) => {
+  const lines = getLines(rules);
+  let rulesObj = {};
+
+  for (let line of lines) {
+    const [name, parts] = makeRule(splitRule(line));
+    rulesObj[name] = parts;
+  }
+
+  return rulesObj;
+};
